86bd0aed04dee415f54f682d1f0038939544cef2
Abhishek Gidde <abhishekct12@gmail.com>
2024-11-19 18:45:44 +0530

    Initial commit with CI/CD setup

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,35 +1,5 @@
-// src/setupTests.js
+// jest-dom adds custom jest matchers for asserting on DOM nodes.
+// allows you to do things like:
+// expect(element).toHaveTextContent(/react/i)
+// learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
-
-// Mock canvas
-class MockCanvas {
-  getContext() {
-    return {
-      fillStyle: '',
-      fillRect: () => {},
-      drawImage: () => {},
-      getImageData: () => ({
-        data: new Uint8ClampedArray(0),
-      }),
-      putImageData: () => {},
-      createImageData: () => {},
-      setTransform: () => {},
-      drawFocus: () => {},
-      beginPath: () => {},
-      moveTo: () => {},
-      lineTo: () => {},
-      stroke: () => {},
-      fill: () => {},
-      rect: () => {},
-      arc: () => {},
-    };
-  }
-}
-
-// Setup canvas mock
-global.HTMLCanvasElement.prototype.getContext = function() {
-  return new MockCanvas().getContext();
-};
-
-// Mock react-lottie
-jest.mock('react-lottie');
